30f818b470c4f200695c178a51dc3ac55641f7d6
akhilaanilkumar2069 <akhilaanilkumar2069@gmail.com>
2026-03-29 11:27:28 +0530

    Update TransactionController.java

--- Transaction Service (Spring Boot)/src/main/java/com/example/demo/TransactionController.java
+++ Transaction Service (Spring Boot)/src/main/java/com/example/demo/TransactionController.java
@@ -1,1 +1,25 @@
 
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TransactionController {
+
+    @PostMapping("/transaction")
+    public Map<String, String> processTransaction(@RequestBody Map<String, Object> payload) {
+
+        // Call Notification Service
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.postForObject("http://notification-service:5000/notify", payload, String.class);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "Transaction Successful");
+
+        return response;
+    }
+}
